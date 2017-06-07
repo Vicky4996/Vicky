2dabac8c261c80d06e45199b6732a5eb58c03415
Vicky4996 <vickymastero96@gmail.com>
2017-06-07 07:36:26 +0530

    Rev.java

---
+++ Rev.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Rev{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+String str=s.nextLine();
+String rts=new StringBuffer(str).reverse().toString;
+System.out.println(rts);
+}
+}
