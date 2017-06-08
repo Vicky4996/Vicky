9414d86b4484c670ed85556b9c78b095e41bea2c
Vicky4996 <vickymastero96@gmail.com>
2017-06-08 18:38:06 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class oddeven {
+	public static void main(String[] args) {
+int a[],i,j=0,k=0,e=0,o=0;
+a=new int[args.length];
+for(i=0;i<args.length;i++)
+{
+	a[i]=Integer.parseInt(args[i]);
+}
+for(i=0;i<args.length;i++)
+{
+	if(a[i]%2==0)
+		{k++;
+	e=a[i];}
+	if(a[i]%2!=0)
+	{j++;
+	o=a[i];}
+}
+if(k==1)
+{
+	System.out.print(e);
+	System.exit(0);
+}
+if(j==1)
+{
+	System.out.print(o);
+	System.exit(0);
+}
+}	}
