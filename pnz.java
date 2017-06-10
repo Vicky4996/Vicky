d18350abf3aa4526babfb605e15b5a6cc01de296
Vicky4996 <vickymastero96@gmail.com>
2017-06-10 21:54:52 +0530

    Create pnz.java

--- pnz.java
+++ pnz.java
@@ -5,9 +5,10 @@ public static void main(String args[])
 Scanner s=new Scanner(System.in);
 int k=s.nextInt();
 if(k>0)
-System.out.print("POSITIVE");
+{System.out.print("POSITIVE");System.exit(0);}
 if(k<0)
-System.out.print("NEGATIVE");
+{System.out.print("NEGATIVE");System.exit(0);}
 if(k==0)
-System.out.print("ZERO");
+{System.out.print("ZERO");System.exit(0);}
+  System.out.print("Invalid Input");
 }}
