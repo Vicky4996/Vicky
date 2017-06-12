bc941f80fcce33375b7357764ec6d2fa8eb345d3
Vicky4996 <vickymastero96@gmail.com>
2017-06-13 00:38:00 +0530

    Create odd.java

--- odd.java
+++ odd.java
@@ -3,8 +3,12 @@ public class odd{
 public static void main(String args[]){
 Scanner s=new Scanner(System.in);
 int k=s.nextInt();
+  if(k>0)
+  {
 if(k%2==0)
 System.out.print("EVEN");
 else
-System.out.print("ODD");
+System.out.print("ODD");}
+  else
+    System.out.print("INVALID INPUT");
 }}
