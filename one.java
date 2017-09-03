0d649b12936a4a6c8382625a709200c34954e62b
Vicky4996 <vickymastero96@gmail.com>
2017-09-03 23:34:59 +0530

    Create one.java

---
+++ one.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class one{
+public static void main(String args[])
+{
+	Scanner s=new Scanner(System.in);
+	String[] st=s.nextLine().split(" ");
+	List <String> al1=new ArrayList <String>(Arrays.asList(st));
+	for(int i=0;i<al1.size();i++)
+	{
+		int k=Collections.frequency(al1, ((List<String>) al1).get(i));
+		if(k==1)
+			{System.out.print(((List<String>) al1).get(i));break;}
+	}
+	
+}
+}
